48702ed945b41cc29a4846932b496efef0cc1814
AnsariKirdar <anwerahmed1771998@gmail.com>
2020-04-19 03:04:18 +0530

    Kirdar

--- edwings.sql
+++ edwings.sql
@@ -7,9 +7,11 @@
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.2.29
 CREATE Database `edwings`;
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE
+= "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
-SET time_zone = "+00:00";
+SET time_zone
+= "+05:30";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -27,20 +29,30 @@ SET time_zone = "+00:00";
 -- Table structure for table `continent`
 --
 
-CREATE TABLE `continent` (
-  `id` int(11) NOT NULL,
-  `continents` varchar(255) DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  `c_by` varchar(255) DEFAULT NULL,
-  `e_by` varchar(255) DEFAULT NULL,
-  `c_date` datetime DEFAULT current_timestamp()
+CREATE TABLE `continent`
+(
+  `id` int
+(11) NOT NULL,
+  `continents` varchar
+(255) DEFAULT NULL,
+  `description` varchar
+(255) DEFAULT NULL,
+  `c_by` varchar
+(255) DEFAULT NULL,
+  `e_by` varchar
+(255) DEFAULT NULL,
+  `c_date` datetime DEFAULT current_timestamp
+()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `continent`
 --
 
-INSERT INTO `continent` (`id`, `continents`, `description`, `c_by`, `e_by`, `c_date`) VALUES
+INSERT INTO `continent` (`
+id`,
+`continents
+`, `description`, `c_by`, `e_by`, `c_date`) VALUES
 (8, 'Europe', 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum, nam eius tenetur distinctio perferendis modi ab necessitatibus beatae minus deserunt, maxime inventore quidem facere totam veniam sunt voluptate fuga porro dicta? Dolorem quasi error volupta', 'Kirdar', 'Kirdar', '2020-04-16 06:03:00'),
 (10, 'South America', 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum, nam eius tenetur distinctio perferendis modi ab necessitatibus beatae minus deserunt, maxime inventore quidem facere totam veniam sunt voluptate fuga porro dicta? Dolorem quasi error volupta', 'Kirdar', 'Kirdar', '2020-04-16 06:03:20'),
 (12, 'Asia', 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum, nam eius tenetur distinctio perferendis modi ab necessitatibus beatae minus deserunt, maxime inventore quidem facere totam veniam sunt voluptate fuga porro dicta? Dolorem quasi error volupta', 'Kirdar', 'Kirdar', '2020-04-16 06:03:37'),
@@ -55,21 +67,32 @@ INSERT INTO `continent` (`id`, `continents`, `description`, `c_by`, `e_by`, `c_d
 -- Table structure for table `country`
 --
 
-CREATE TABLE `country` (
-  `id` int(11) NOT NULL,
-  `continents` varchar(255) DEFAULT NULL,
-  `country` varchar(255) DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  `c_by` varchar(255) DEFAULT NULL,
-  `e_by` varchar(255) DEFAULT NULL,
-  `c_date` datetime DEFAULT current_timestamp()
+CREATE TABLE `country`
+(
+  `id` int
+(11) NOT NULL,
+  `continents` varchar
+(255) DEFAULT NULL,
+  `country` varchar
+(255) DEFAULT NULL,
+  `description` varchar
+(255) DEFAULT NULL,
+  `c_by` varchar
+(255) DEFAULT NULL,
+  `e_by` varchar
+(255) DEFAULT NULL,
+  `c_date` datetime DEFAULT current_timestamp
+()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `country`
 --
 
-INSERT INTO `country` (`id`, `continents`, `country`, `description`, `c_by`, `e_by`, `c_date`) VALUES
+INSERT INTO `country` (`
+id`,
+`continents
+`, `country`, `description`, `c_by`, `e_by`, `c_date`) VALUES
 (1, 'Europe', 'Austria', 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum, nam eius tenetur distinctio perferendis modi ab necessitatibus beatae minus deserunt, maxime inventore quidem facere totam veniam sunt voluptate fuga porro dicta? Dolorem quasi error volupta', 'Kirdar', NULL, '2020-04-16 06:37:00'),
 (2, 'Europe', 'Belgium', 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum, nam eius tenetur distinctio perferendis modi ab necessitatibus beatae minus deserunt, maxime inventore quidem facere totam veniam sunt voluptate fuga porro dicta? Dolorem quasi error volupta', 'Kirdar', NULL, '2020-04-16 06:44:33'),
 (3, 'Europe', 'France', 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum, nam eius tenetur distinctio perferendis modi ab necessitatibus beatae minus deserunt, maxime inventore quidem facere totam veniam sunt voluptate fuga porro dicta? Dolorem quasi error volupta', 'Kirdar', NULL, '2020-04-16 06:46:03'),
@@ -93,28 +116,49 @@ INSERT INTO `country` (`id`, `continents`, `country`, `description`, `c_by`, `e_
 -- Table structure for table `customer_feedback`
 --
 
-CREATE TABLE `customer_feedback` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `office_name` varchar(255) DEFAULT NULL,
-  `next_destination` varchar(255) DEFAULT NULL,
-  `destination_visited` varchar(255) DEFAULT NULL,
-  `departure_date` varchar(30) DEFAULT NULL,
-  `telephone` varchar(20) DEFAULT NULL,
-  `mobile` varchar(20) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `travel_again` varchar(3) DEFAULT NULL,
-  `recommend_friends` varchar(3) DEFAULT NULL,
+CREATE TABLE `customer_feedback`
+(
+  `id` int
+(11) NOT NULL,
+  `name` varchar
+(255) DEFAULT NULL,
+  `office_name` varchar
+(255) DEFAULT NULL,
+  `next_destination` varchar
+(255) DEFAULT NULL,
+  `destination_visited` varchar
+(255) DEFAULT NULL,
+  `departure_date` varchar
+(30) DEFAULT NULL,
+  `telephone` varchar
+(20) DEFAULT NULL,
+  `mobile` varchar
+(20) DEFAULT NULL,
+  `email` varchar
+(255) DEFAULT NULL,
+  `travel_again` varchar
+(3) DEFAULT NULL,
+  `recommend_friends` varchar
+(3) DEFAULT NULL,
   `suggestions` longtext DEFAULT NULL,
-  `ip` varchar(30) DEFAULT NULL,
-  `c_date` datetime NOT NULL DEFAULT current_timestamp()
+  `ip` varchar
+(30) DEFAULT NULL,
+  `c_date` datetime NOT NULL DEFAULT current_timestamp
+()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `customer_feedback`
 --
 
-INSERT INTO `customer_feedback` (`id`, `name`, `office_name`, `next_destination`, `destination_visited`, `departure_date`, `telephone`, `mobile`, `email`, `travel_again`, `recommend_friends`, `suggestions`, `ip`, `c_date`) VALUES
+INSERT INTO `customer_feedback` (`
+id`,
+`name`,
+`office_name`,
+`next_destination`,
+`destination_visited`,
+`departure_date
+`, `telephone`, `mobile`, `email`, `travel_again`, `recommend_friends`, `suggestions`, `ip`, `c_date`) VALUES
 (1, 'Kirdar', 'XYZ', NULL, 'America', '2020-04-03', 'Telephone', 'Mobile', 'anwerahmed1771998@gmail.com', 'on', 'on', 'aaaaaa', '', '2020-04-03 01:18:53'),
 (2, 'Kirdar', 'XYZ', NULL, 'America', '2020-04-03', 'Telephone', 'Mobile', 'anwerahmed1771998@gmail.com', 'on', 'on', 'aaaaaa', '', '2020-04-03 01:19:29'),
 (3, 'Kirdar', 'XYZ', NULL, 'America', '2020-04-03', 'Telephone', 'Mobile', 'anwerahmed1771998@gmail.com', 'on', 'on', 'aaaaaa', '', '2020-04-03 01:20:35'),
@@ -128,21 +172,30 @@ INSERT INTO `customer_feedback` (`id`, `name`, `office_name`, `next_destination`
 -- Table structure for table `gallery`
 --
 
-CREATE TABLE `gallery` (
-  `id` int(11) NOT NULL,
-  `title` varchar(255) DEFAULT NULL,
+CREATE TABLE `gallery`
+(
+  `id` int
+(11) NOT NULL,
+  `title` varchar
+(255) DEFAULT NULL,
   `thumb` longtext DEFAULT NULL,
   `full_img` longtext DEFAULT NULL,
-  `u_by` varchar(255) DEFAULT NULL,
-  `e_date` datetime DEFAULT current_timestamp(),
-  `c_date` datetime DEFAULT current_timestamp()
+  `u_by` varchar
+(255) DEFAULT NULL,
+  `e_date` datetime DEFAULT current_timestamp
+(),
+  `c_date` datetime DEFAULT current_timestamp
+()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `gallery`
 --
 
-INSERT INTO `gallery` (`id`, `title`, `thumb`, `full_img`, `u_by`, `e_date`, `c_date`) VALUES
+INSERT INTO `gallery` (`
+id`,
+`title
+`, `thumb`, `full_img`, `u_by`, `e_date`, `c_date`) VALUES
 (7, 'Taj Mahal', ' 444c5fa7e80ce8facf2c8aad052f1b26a.jpg', ' 444c5fa7e80ce8facf2c8aad052f1b26a.jpg', 'Kirdar', '2020-04-18 23:53:48', '0000-00-00 00:00:00'),
 (8, 'lorem', ' 2a069651f789df22f60e57534f038f4ea.jpg', ' 2a069651f789df22f60e57534f038f4ea.jpg', 'Kirdar', '2020-04-18 23:54:03', '0000-00-00 00:00:00'),
 (9, 'lorem', ' c79689cbf24d39a5d7bb50b9a94a0e98a.jpg', ' c79689cbf24d39a5d7bb50b9a94a0e98a.jpg', 'Kirdar', '2020-04-18 23:54:33', '0000-00-00 00:00:00'),
@@ -160,24 +213,36 @@ INSERT INTO `gallery` (`id`, `title`, `thumb`, `full_img`, `u_by`, `e_date`, `c_
 -- Table structure for table `login`
 --
 
-CREATE TABLE `login` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `username` varchar(255) DEFAULT NULL,
+CREATE TABLE `login`
+(
+  `id` int
+(11) NOT NULL,
+  `name` varchar
+(255) DEFAULT NULL,
+  `username` varchar
+(255) DEFAULT NULL,
   `password` longtext DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `f_password` int(8) DEFAULT NULL,
-  `type` varchar(255) DEFAULT NULL,
-  `trys` int(1) DEFAULT NULL,
+  `email` varchar
+(255) DEFAULT NULL,
+  `f_password` int
+(8) DEFAULT NULL,
+  `type` varchar
+(255) DEFAULT NULL,
+  `trys` int
+(1) DEFAULT NULL,
   `trys_out` datetime DEFAULT NULL,
-  `c_date` datetime DEFAULT current_timestamp()
+  `c_date` datetime DEFAULT current_timestamp
+()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `login`
 --
 
-INSERT INTO `login` (`id`, `name`, `username`, `password`, `email`, `f_password`, `type`, `trys`, `trys_out`, `c_date`) VALUES
+INSERT INTO `login` (`
+id`,
+`name
+`, `username`, `password`, `email`, `f_password`, `type`, `trys`, `trys_out`, `c_date`) VALUES
 (1, 'Kirdar', 'Kirdar', '202cb962ac59075b964b07152d234b70', 'anwerahmed1771998@gmail.com', 123, 'admin', 4, '2020-03-30 02:14:21', '2020-03-30 02:44:21');
 
 -- --------------------------------------------------------
@@ -186,23 +251,36 @@ INSERT INTO `login` (`id`, `name`, `username`, `password`, `email`, `f_password`
 -- Table structure for table `slider`
 --
 
-CREATE TABLE `slider` (
-  `id` int(11) NOT NULL,
-  `title` varchar(255) DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  `link` varchar(255) DEFAULT NULL,
-  `link_text` varchar(255) DEFAULT NULL,
-  `file` varchar(255) DEFAULT NULL,
-  `c_by` varchar(255) DEFAULT NULL,
-  `e_by` varchar(255) DEFAULT NULL,
-  `c_date` datetime DEFAULT current_timestamp()
+CREATE TABLE `slider`
+(
+  `id` int
+(11) NOT NULL,
+  `title` varchar
+(255) DEFAULT NULL,
+  `description` varchar
+(255) DEFAULT NULL,
+  `link` varchar
+(255) DEFAULT NULL,
+  `link_text` varchar
+(255) DEFAULT NULL,
+  `file` varchar
+(255) DEFAULT NULL,
+  `c_by` varchar
+(255) DEFAULT NULL,
+  `e_by` varchar
+(255) DEFAULT NULL,
+  `c_date` datetime DEFAULT current_timestamp
+()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `slider`
 --
 
-INSERT INTO `slider` (`id`, `title`, `description`, `link`, `link_text`, `file`, `c_by`, `e_by`, `c_date`) VALUES
+INSERT INTO `slider` (`
+id`,
+`title
+`, `description`, `link`, `link_text`, `file`, `c_by`, `e_by`, `c_date`) VALUES
 (8, 'Mountain Climbing', 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum, nam eius tenetur distinctio perferendis modi ab necessitatibus beatae minus deserunt, maxime inventore quidem facere totam veniam sunt voluptate fuga porro dicta? Dolorem quasi error volupta', 'Kirdar', 'Best Deal', 'b898dd2511c21ca869bd3aca3d9f70c6photo-1508558936510-0af1e3cccbab.jpg', '', 'Kirdar', '0000-00-00 00:00:00'),
 (13, 'Dubai', 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum, nam eius tenetur distinctio perferendis modi ab necessitatibus beatae minus deserunt, maxime inventore quidem facere totam veniam sunt voluptate fuga porro dicta? Dolorem quasi error volupta', 'Kirdar', 'Book Now', 'e3d426fb2cc81c5fc06cd1bf99347523photo-1462007895615-c8c073bebcd8.jpg', '', 'Kirdar', '0000-00-00 00:00:00'),
 (16, 'Prias', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, corrupti natus reprehenderit culpa dolores nisi quidem sapiente, ipsam sunt voluptatem odio quibusdam? Pariatur deserunt quae facilis molestiae saepe est. Maiores.', 'Kirdar', 'Book Now', '0a34dfe740ebdb6607e8b25eaa9a03b9photo-1502602898657-3e91760cbb34.png', 'Kirdar', 'Kirdar', '2020-04-16 04:28:46'),
@@ -216,37 +294,43 @@ INSERT INTO `slider` (`id`, `title`, `description`, `link`, `link_text`, `file`,
 -- Indexes for table `continent`
 --
 ALTER TABLE `continent`
-  ADD PRIMARY KEY (`id`);
+ADD PRIMARY KEY
+(`id`);
 
 --
 -- Indexes for table `country`
 --
 ALTER TABLE `country`
-  ADD PRIMARY KEY (`id`);
+ADD PRIMARY KEY
+(`id`);
 
 --
 -- Indexes for table `customer_feedback`
 --
 ALTER TABLE `customer_feedback`
-  ADD PRIMARY KEY (`id`);
+ADD PRIMARY KEY
+(`id`);
 
 --
 -- Indexes for table `gallery`
 --
 ALTER TABLE `gallery`
-  ADD PRIMARY KEY (`id`);
+ADD PRIMARY KEY
+(`id`);
 
 --
 -- Indexes for table `login`
 --
 ALTER TABLE `login`
-  ADD PRIMARY KEY (`id`);
+ADD PRIMARY KEY
+(`id`);
 
 --
 -- Indexes for table `slider`
 --
 ALTER TABLE `slider`
-  ADD PRIMARY KEY (`id`);
+ADD PRIMARY KEY
+(`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -256,37 +340,43 @@ ALTER TABLE `slider`
 -- AUTO_INCREMENT for table `continent`
 --
 ALTER TABLE `continent`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int
+(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `country`
 --
 ALTER TABLE `country`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int
+(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `customer_feedback`
 --
 ALTER TABLE `customer_feedback`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int
+(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `gallery`
 --
 ALTER TABLE `gallery`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int
+(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `login`
 --
 ALTER TABLE `login`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int
+(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `slider`
 --
 ALTER TABLE `slider`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int
+(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
